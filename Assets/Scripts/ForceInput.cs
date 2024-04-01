c8e71b0b6712f67a5368172d62ce3eb79798cf9e
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2024-04-01 15:20:30 +0200

    :rocket: first commit

---
+++ Assets/Scripts/ForceInput.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForceInput : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float ballspeed;
+	public GUIStyle style;
+	public int score = -10;
+	private GameObject Can;
+	private bool vert_test;
+	private Quaternion localQ;
+	private float startTime = 0.0f;
+    float angle = 0.0f;
+    Vector3 axis = Vector3.zero;
+    public float slider_v;
+    public Slider speed_slider;
+
+	// array of booleans, one per can: not scored yet
+    public bool[] scored = {false,false,false,false,false,false,false};
+
+	// On choisit la taille et l'emplacement où sera affiché le score
+	void OnGUI()
+	{
+		float x = Screen.width / 2f;
+		float y = 30f;
+		float width = 300f;
+		float height = 20f;
+
+		if (speed_slider)
+		{
+			ballspeed = slider_v;
+		}
+
+		string text_speed = "Ball Speed: " + ballspeed.ToString();
+		string text_score = "Score = " + score.ToString();
+
+		style.fontSize = 30;
+		GUI.Label(new Rect(x - (width / 2f), y, width, height), text_speed, style);
+		GUI.Label(new Rect(x - (width / 2f), y + 30f, width, height), text_score, style);
+
+		if (score == 60)
+		{
+			string game_over = "GAME OVER, BRAVO !!";
+			GUI.Label(new Rect(x - (width / 2f), y + 100f, width, height), game_over, style);
+			
+		}
+	}
+
+    public void slide_change (float value)
+    {
+		// Debug.Log("value changed!");
+            slider_v = value;
+    }
+
+	void Start () {
+
+		if (speed_slider)
+		{
+			// add callback
+			speed_slider.onValueChanged.AddListener(slide_change);
+			// initial value (callback not called yet)
+			slider_v = speed_slider.value;
+		}
+
+		for (int i = 1; i <= 6; i++) {
+			scored [i] = false;
+		}
+		score = 0;
+		startTime = 0.0f;
+        
+    }
+		void Update() {
+
+		// wait until some time has elapsed ...
+		if (startTime > 1.0f) {
+			float roulette = (float)Input.GetAxisRaw ("Mouse ScrollWheel");
+			
+			if (speed_slider)
+            {
+				// get speed from slider
+				ballspeed = slider_v;
+            }
+			else
+            {
+				// get it from roulette
+				ballspeed += roulette;
+			}
+
+			// no negative speed
+			if (ballspeed < 0)
+				ballspeed = 0;
+
+			// for each Can: if moved enough from vertical, then add 10 to score and mark as moved
+			for (int i = 1; i <= 6; i++) {
+				// Debug.Log("finding Can:" + "Can" + i.ToString());
+				Can = GameObject.Find ("Can" + i.ToString ());
+                localQ = Can.transform.rotation;
+                localQ.ToAngleAxis(out angle, out axis);
+
+				float magRot = Mathf.Abs (localQ.eulerAngles.x) + Mathf.Abs (localQ.eulerAngles.z);
+				if (Can != null && i == 1)
+                {
+                  // Debug.Log ("Can " + i + " axis= " + axis + " angle= " + angle);
+                }
+                // verticality test
+                vert_test = true;
+				if (magRot > 10 && magRot < 350)
+					vert_test = false;
+				if (magRot < -10 && magRot > -350)
+					vert_test = false;
+				if (vert_test == false && scored [i] == false) {
+					//Debug.Log ("Can " + i + " has scored");
+					//Debug.Log ("localQ=" + magRot);
+					score += 10;
+					scored [i] = true;
+				}
+			}
+		}
+		startTime += Time.deltaTime;
+
+	}
+}
---
+++ Assets/Scripts/launch.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launch : MonoBehaviour {
+
+	public Vector3 init_speed = new Vector3(-1.5f, 0f,0f);
+	private float fallTime;
+	private float shotTime;
+	private Rigidbody rbGO;
+	private GameObject arrow;
+	private bool started;
+	public ForceInput force;
+	public Vector3 init_pos;
+	private bool triggered;
+	private bool reset;
+    protected bool fire_button = false;
+    protected JoyButton joybutton;
+    protected Joystick joystick;
+    private GameObject Can;
+	bool disable_throw = false;
+
+	// Use this for initialization
+	void Start () {
+		//You get the Rigidbody component you attach to the GameObject
+		fallTime = 0.0f;
+		shotTime = 0.0f;
+		started = false;
+		triggered = false;
+		disable_throw = false;
+
+		reset = false;
+		rbGO = GetComponent<Rigidbody>();
+		arrow = GameObject.FindGameObjectsWithTag("Arrow")[0];
+		init_pos = transform.localPosition;
+        rbGO.useGravity = false;
+		
+        joybutton = FindObjectOfType<JoyButton>();
+
+		AllReset();
+	}
+
+	public void AllReset() {
+
+		// Debug.Log("Reset!");
+		// all Cans back on line
+		for (int i = 1; i <= 6; i++) {
+			 Can = GameObject.Find ("Can" + i.ToString ());
+			Rigidbody rbb = Can.GetComponent<Rigidbody>();
+
+			if (i==1)
+				Can.transform.position = new Vector3 (-0.5f, 0.3f, 0f);
+			if (i==2)
+				Can.transform.position = new Vector3 (0f, 0.3f, 0f);
+			if (i==3)
+				Can.transform.position = new Vector3 (0.5f, 0.3f, 0f);
+			if (i==4)
+				Can.transform.position = new Vector3 (-0.25f, 0.72f, 0f);
+			if (i==5)
+				Can.transform.position = new Vector3 (0.25f, 0.72f, 0f);
+			if (i==6)
+				Can.transform.position = new Vector3 (0f, 1.15f, 0f);
+			
+
+			Can.transform.rotation = Quaternion.identity;    
+			rbb.velocity = Vector3.zero;
+			rbb.angularVelocity = Vector3.zero;
+		}
+		// score back to zero and reset can state
+		force.score = 0;
+		for (int i = 1; i <= 6; i++)
+		{
+			force.scored[i] = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		
+		started = Input.GetMouseButtonDown (1);
+        if (joybutton)
+        {
+            if (!fire_button && joybutton.pressed && !disable_throw)
+            {
+                fire_button = true;
+				disable_throw = false;
+                started = true;
+            }
+
+            // stop "fire" action
+            if (fire_button && !joybutton.pressed)
+                fire_button = false;
+        }
+
+        reset = Input.GetMouseButtonDown (2);
+
+		// check for ball speed, if above threshold, start timer
+		if (Vector3.Magnitude(rbGO.velocity) > 0.01 && triggered == false) {
+			// Debug.Log ("triggered");
+			triggered = true;
+		}
+		
+		
+		if (reset) {
+			AllReset ();
+		}
+		// if time has started you can start playing
+		if (fallTime > 0.0f)
+		{
+			if (started && !disable_throw)
+			{
+				// if "fire" pressed (should be called once)
+				shotTime = 0.0f;
+				triggered = false;
+				rbGO.WakeUp();
+				rbGO.useGravity = true;
+				// compute 3d speed vector as unit vector oriented like parent arrow
+				// multiplied by ballspeed intensity
+				Vector3 Xaxis = new Vector3(0,1,0);
+				// compute direction of arrow
+				Vector3 RotatedX = arrow.transform.rotation * Xaxis;
+				// multiply vector by speed magnitude to get speed vector
+				RotatedX *= -force.ballspeed;
+				// Debug.Log ("speed=" + RotatedX);
+				rbGO.AddForce(RotatedX, ForceMode.Impulse);
+				started = false;
+				// disable actions on ball during throw
+				disable_throw = true;
+				// Debug.Log("throw disabled");
+			}
+		}
+		else
+		{
+			rbGO.Sleep();
+			//Debug.Log("sleep");
+		}
+
+		fallTime += Time.deltaTime;
+
+		if (triggered) {
+			shotTime += Time.deltaTime;
+			// Debug.Log("Time =" + shotTime);
+			if (shotTime > 5.0f) {
+				transform.localPosition = init_pos;
+				rbGO.Sleep ();
+				rbGO.useGravity = false;
+				triggered = false;
+				shotTime = 0.0f;
+				disable_throw = false;
+			}
+		}
+	}
+
+	public void QuitApp()
+    {
+		Debug.Log("Quitting the app !");
+        Application.Quit();
+    }
+}
